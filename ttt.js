49141ef497eacce25ce31c3eac44ab36238fe09d
Arslan Ashraf <arslanash@gmail.com>
2018-09-14 00:29:41 -0400

    css add

--- ttt.js
+++ ttt.js
@@ -1,1 +1,11 @@
 console.log('slfdksdj')
+
+let player_one = 'X'
+let player_two = 'O'
+let current_turn = player_one
+let board = document.querySelector('.board')
+
+board.addEventListener('click', function(e){
+	e.target.innerHTML = current_turn
+	current_turn = current_turn == player_one ? player_two : player_one
+})
